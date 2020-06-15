6fb85233599aa887b621779233aa82f7223779e7
Omar Masoud <omarmasoudonline@gmail.com>
2020-06-15 21:05:17 +0200

    Made the landing page responsive and fixed the HTML structure

---
+++ resources/javascript/menu.js
@@ -0,0 +1,8 @@
+var menuIcon = document.getElementById('menu-icon');
+var menu = document.querySelector("nav")
+
+menuIcon.addEventListener('click', function(){
+    
+    menu.classList.toggle("open-nav")
+    
+})
